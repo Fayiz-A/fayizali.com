56f61f16de38975b413f491a15c7d39a414d3d2d
Fayiz-A <68380206+Fayiz-A@users.noreply.github.com>
2021-01-14 17:08:45 +0530

    Made some of the code reusable

--- lib/routes/computer_languages_page.dart
+++ lib/routes/computer_languages_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:ui' as ui;
+import 'dart:math' as Math;
 
 class ComputerLanguagesPage extends StatelessWidget {
   @override
@@ -19,12 +20,6 @@ class ComputerLanguagesPage extends StatelessWidget {
 class FramePainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
-    Offset center = Offset(size.width / 2, size.height / 2);
-    Offset centerLeft = Offset(0, center.dy);
-    Offset topCenter = Offset(center.dx, 0);
-    Offset topRightCorner = Offset(size.width, 0.0);
-    Offset topLeftCorner = Offset(0, 0);
-
     Map<String, Offset> mainCoordinatesMap = {
       'topLeft': Offset(0, 0),
       'topCenter': Offset(size.width / 2, 0),
@@ -39,19 +34,66 @@ class FramePainter extends CustomPainter {
 
     Paint topCurvePaint = Paint()
       ..shader = ui.Gradient.linear(
-          Offset(0, 0), Offset(200, 200), [Colors.orange, Colors.deepOrange]);
+          Offset(0, 0),
+          Offset(mainCoordinatesMap['bottomLeft'].dx,
+              mainCoordinatesMap['bottomLeft'].dy),
+          [Colors.orange, Colors.deepOrange]);
 
     Path topCurvePath = Path()
-      ..moveTo(mainCoordinatesMap['topLeft'].dx, mainCoordinatesMap['topLeft'].dy) //just to be safe else this is the default value
-      ..lineTo(mainCoordinatesMap['topLeft'].dx, mainCoordinatesMap['topLeft'].dy + size.height*0.33) //this is done for the starting point of the curve
+      ..moveTo(
+          mainCoordinatesMap['topLeft'].dx,
+          mainCoordinatesMap['topLeft']
+              .dy) //just to be safe else this is the default value
+      ..lineTo(
+          mainCoordinatesMap['topLeft'].dx,
+          size.height > 850
+              ? mainCoordinatesMap['topLeft'].dy + size.height * 0.33
+              : mainCoordinatesMap['topLeft'].dy +
+                  size.height *
+                      0.35) //this is done for the starting point of the curve
+      //these hardcoded values do not effect app responsiveness
       ..quadraticBezierTo(90, 240, 90, 200)
       ..quadraticBezierTo(110, 40, 250, 30)
       ..quadraticBezierTo(280, 25, 290, 0)
       ..close();
 
     canvas.drawPath(topCurvePath, topCurvePaint);
+
+    double bottomCenterArcRadius = size.height * 0.18;
+    Offset bottomCenterArcCenter = Offset(
+        mainCoordinatesMap['bottomLeft'].dx + bottomCenterArcRadius * 1.7,
+        mainCoordinatesMap['bottomLeft'].dy + (bottomCenterArcRadius * 0.25));
+
+    drawHalfCircleWithOutlineCircle(canvas: canvas, innerCircleRadius: bottomCenterArcRadius, radiusDifference: 50, innerCircleColor: Colors.red, outerCircleColor: Colors.blue, center: bottomCenterArcCenter, mainCoordinatesMap: mainCoordinatesMap);
   }
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
+
+  void drawHalfCircle(
+      {@required Canvas canvas,
+      @required double radius,
+      Color color,
+      @required Offset center,
+      @required Map<String, Offset> mainCoordinatesMap}) {
+    Paint halfCirclePaint = Paint()..color = color ?? Colors.white;
+
+    Rect halfCircleRect = Rect.fromCircle(center: center, radius: radius);
+
+    canvas.drawArc(halfCircleRect, 0, -Math.pi, true, halfCirclePaint);
+  }
+
+  void drawHalfCircleWithOutlineCircle(
+    {@required Canvas canvas,
+      @required double innerCircleRadius,
+      @required double radiusDifference,
+      @required Color innerCircleColor,
+      @required Color outerCircleColor,
+      @required Offset center,
+      @required Map<String, Offset> mainCoordinatesMap}
+  ) {
+    drawHalfCircle(canvas: canvas, radius: innerCircleRadius + radiusDifference, center: center, mainCoordinatesMap: mainCoordinatesMap, color: outerCircleColor);
+    drawHalfCircle(canvas: canvas, radius: innerCircleRadius, center: center, mainCoordinatesMap: mainCoordinatesMap, color: innerCircleColor);
+
+  }
 }
