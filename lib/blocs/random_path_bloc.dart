08249fd43da5d530371ca5dfc196c016788c19c4
Fayiz-A <68380206+Fayiz-A@users.noreply.github.com>
2021-02-08 18:59:57 +0530

    Added bubble repeat effect

--- lib/blocs/random_path_bloc.dart
+++ lib/blocs/random_path_bloc.dart
@@ -11,61 +11,194 @@ class RandomPathGeneratorEvent extends RandomPathEvent {
   final Size windowSize;
   final int numberOfPaths;
 
-  RandomPathGeneratorEvent({@required this.windowSize, @required this.numberOfPaths});
+  RandomPathGeneratorEvent(
+      {@required this.windowSize, @required this.numberOfPaths});
+}
+
+class PathRelatedToPreviousPathsGeneratorEvent extends RandomPathEvent {
+  final Size windowSize;
+  final int numberOfPaths;
+  final List<Path> pathList;
+
+  PathRelatedToPreviousPathsGeneratorEvent(
+      {@required this.windowSize, @required this.numberOfPaths, @required this.pathList});
 }
 
 class RandomPathBloc extends Bloc<RandomPathEvent, RandomPathState> {
   RandomPathBloc() : super(RandomPathInitialState());
 
-  RandomPathState get initialState => RandomPathInitialState();
-
   @override
-  Stream<RandomPathState> mapEventToState(
-      RandomPathEvent event,
-      ) async* {
+  Stream<RandomPathState> mapEventToState(RandomPathEvent event,) async* {
     if (event is RandomPathGeneratorEvent) {
-      if(state is RandomPathInitialState) {
-        Size windowSize = event.windowSize;
+      Size windowSize = event.windowSize;
 
-        List<Path> pathList = [];
-        
-        for(int pathIndex = 0; pathIndex<event.numberOfPaths; pathIndex++) {
-          math.Random random = math.Random();
+      List<Path> pathList = [];
 
-          double randomX = random.nextDouble();
-          double randomXInReasonableRange = _mapValueFromOneRangeToAnother(value: randomX, range1Min: 0.0,  range1Max: 1.0, range2Min: 0.0, range2Max: windowSize.width );
+      for (int pathIndex = 0; pathIndex < event.numberOfPaths; pathIndex++) {
+        math.Random random = math.Random();
 
-          double randomY = random.nextDouble();
-          double randomYInReasonableRange = _mapValueFromOneRangeToAnother(value: randomY, range1Min: 0.0,  range1Max: 1.0, range2Min: windowSize.height, range2Max: windowSize.height * 2 );
+        double randomX = random.nextDouble();
+        double randomXInReasonableRange = _mapValueFromOneRangeToAnother(
+            value: randomX,
+            range1Min: 0.0,
+            range1Max: 1.0,
+            range2Min: 0.0,
+            range2Max: windowSize.width
+        );
+
+        double randomY = random.nextDouble();
+        double randomYInReasonableRange = _mapValueFromOneRangeToAnother(
+            value: randomY,
+            range1Min: 0.0,
+            range1Max: 1.0,
+            range2Min: windowSize.height,
+            range2Max: windowSize.height * 2
+        );
+
+        double randomLineEndX = random.nextDouble();
+        double randomLineEndXInReasonableRange = _mapValueFromOneRangeToAnother(
+            value: randomLineEndX,
+            range1Min: 0.0,
+            range1Max: 1.0,
+            range2Min: 0.0,
+            range2Max: windowSize.width
+        );
+
+        double randomLineEndY = random.nextDouble();
+        double randomLineEndYInReasonableRange = _mapValueFromOneRangeToAnother(
+            value: randomLineEndY,
+            range1Min: 0.0,
+            range1Max: 1.0,
+            range2Min: -(windowSize.height / 2),
+            range2Max: windowSize.height / 2
+        );
+
+        Path path = Path()
+          ..moveTo(randomXInReasonableRange, randomYInReasonableRange)
+          ..lineTo(randomLineEndXInReasonableRange,
+              randomLineEndYInReasonableRange);
+          // ..close();
+        pathList.add(path);
+      }
+
+      yield RandomPathGeneratedState(pathList: pathList);
+    } else if (event is PathRelatedToPreviousPathsGeneratorEvent) {
+      List<Path> previousPathList = event.pathList;
+      Size windowSize = event.windowSize;
+      List<Path> pathList = [];
+
+      for (int pathIndex = 0; pathIndex <
+          previousPathList.length; pathIndex++) {
+        Path previousPath = previousPathList[pathIndex];
+
+        Offset previousPathEndOffset = _getPathEndOffset(path: previousPath);
+        bool previousPathInWindowBounds = _checkIfPathEndInScreenBounds(pathEndOffset: previousPathEndOffset, windowSize: windowSize);
+
+        math.Random random = math.Random();
+
+        double randomX = random.nextDouble();
+        double randomY = random.nextDouble();
+
+        double randomLineEndX = random.nextDouble();
+        double randomLineEndY = random.nextDouble();
+
+        Path path;
+
+        if (previousPathInWindowBounds) {
+
+          double randomLineEndXInReasonableRange = _mapValueFromOneRangeToAnother(
+              value: randomLineEndX,
+              range1Min: 0.0,
+              range1Max: 1.0,
+              range2Min: 0.0,
+              range2Max: windowSize.width);
+
+          double randomLineEndYInReasonableRange = _mapValueFromOneRangeToAnother(
+              value: randomLineEndY,
+              range1Min: 0.0,
+              range1Max: 1.0,
+              range2Min: -(windowSize.height / 10),
+              range2Max: -(windowSize.height / 2));
+
+
+          path = Path()
+              ..moveTo(previousPathEndOffset.dx, previousPathEndOffset.dy)
+              ..lineTo(randomLineEndXInReasonableRange, randomLineEndYInReasonableRange);
+              // ..close();
+        } else {
+          double randomYInReasonableRange = _mapValueFromOneRangeToAnother(
+              value: randomY,
+              range1Min: 0.0,
+              range1Max: 1.0,
+              range2Min: windowSize.height,
+              range2Max: windowSize.height * 2);
+
+          double randomXInReasonableRange = _mapValueFromOneRangeToAnother(
+              value: randomX,
+              range1Min: 0.0,
+              range1Max: 1.0,
+              range2Min: 0.0,
+              range2Max: windowSize.width);
+
 
           double randomLineEndX = random.nextDouble();
-          double randomLineEndXInReasonableRange = _mapValueFromOneRangeToAnother(value: randomLineEndX, range1Min: 0.0,  range1Max: 1.0, range2Min: 0.0, range2Max: windowSize.width );
+          double randomLineEndXInReasonableRange = _mapValueFromOneRangeToAnother(
+              value: randomLineEndX,
+              range1Min: 0.0,
+              range1Max: 1.0,
+              range2Min: 0.0,
+              range2Max: windowSize.width);
 
           double randomLineEndY = random.nextDouble();
-          double randomLineEndYInReasonableRange = _mapValueFromOneRangeToAnother(value: randomLineEndY, range1Min: 0.0,  range1Max: 1.0, range2Min: 0.0, range2Max: 200 );
+          double randomLineEndYInReasonableRange = _mapValueFromOneRangeToAnother(
+              value: randomLineEndY,
+              range1Min: 0.0,
+              range1Max: 1.0,
+              range2Min: -(windowSize.height / 2),
+              range2Max: windowSize.height / 2)
+          ;
 
-          Path path = Path()
+          path = Path()
             ..moveTo(randomXInReasonableRange, randomYInReasonableRange)
-            ..lineTo(randomLineEndXInReasonableRange, randomLineEndYInReasonableRange)
-            ..close();
-          pathList.add(path);
+            ..lineTo(
+                randomLineEndXInReasonableRange, randomLineEndYInReasonableRange);
+            // ..close();
         }
-        
-        yield RandomPathGeneratedState(pathList: pathList);
+
+        pathList.add(path);
 
       }
+
+      yield RandomPathGeneratedState(pathList: pathList);
     } else {
       yield RandomPathInitialState();
     }
   }
 
-  double _mapValueFromOneRangeToAnother({@required value, @required range1Min, @required range1Max, @required range2Min, @required range2Max}) {
+  double _mapValueFromOneRangeToAnother(
+      {@required value, @required range1Min, @required range1Max, @required range2Min, @required range2Max}) {
     double oldRange = range1Max - range1Min;
     double newRange = range2Max - range2Min;
     double newValue = (((value - range1Min) / oldRange) * newRange) + range2Min;
     
     return newValue;
   }
+
+  Offset _getPathEndOffset({@required Path path}) {
+    PathMetrics pathMetrics = path.computeMetrics();
+    PathMetric pathMetric = pathMetrics.elementAt(0);
+    double valueToTranslateTo = pathMetric.length;
+    Tangent positionDetailsForBubble = pathMetric.getTangentForOffset(valueToTranslateTo);
+
+    Offset position = positionDetailsForBubble.position;
+
+    return position;
+  }
+
+  bool _checkIfPathEndInScreenBounds({@required Offset pathEndOffset, @required Size windowSize}) {
+    return pathEndOffset.dx < windowSize.width && pathEndOffset.dx > 0.0 && pathEndOffset.dy < windowSize.height && pathEndOffset.dy > 0.0;
+  }
+
 }
 
 abstract class RandomPathState {}
@@ -75,4 +208,9 @@ class RandomPathInitialState extends RandomPathState {}
 class RandomPathGeneratedState extends RandomPathState {
   final List<Path> pathList;
   RandomPathGeneratedState({@required this.pathList});
+}
+
+class PathRelatedToPreviousPathsGeneratorState extends RandomPathState {
+  final List<Path> pathList;
+  PathRelatedToPreviousPathsGeneratorState({@required this.pathList});
 }
--- lib/routes/general_info_page.dart
+++ lib/routes/general_info_page.dart
@@ -14,13 +14,25 @@ class GeneralInfoPage extends StatefulWidget {
 class _GeneralInfoPageState extends State<GeneralInfoPage> with SingleTickerProviderStateMixin {
   AnimationController _bubbleOffsetController;
 
+  List<Path> _pathList = [];
+  RandomPathBloc randomPathBloc;
+
   @override
   void initState() {
     super.initState();
     _bubbleOffsetController = AnimationController(
         vsync: this,
-        duration: Duration(milliseconds: 2000)
-    )..forward();
+        duration: Duration(milliseconds: 5000),
+    )
+      ..forward()
+      ..addListener(() {
+        if(_bubbleOffsetController.value == 1.0 && _pathList.isNotEmpty && randomPathBloc != null) {
+
+          randomPathBloc.add(PathRelatedToPreviousPathsGeneratorEvent(windowSize: MediaQuery.of(context).size, numberOfPaths: 20, pathList: _pathList));
+          _bubbleOffsetController.forward(from: 0.0);
+
+        }
+      });
   }
 
   @override
@@ -48,6 +60,7 @@ class _GeneralInfoPageState extends State<GeneralInfoPage> with SingleTickerProv
             builder: (BuildContext context, RandomPathState state) {
               if(state is RandomPathGeneratedState) {
                 List<Path> pathList = state.pathList;
+                _pathList = pathList;//for global access to this path list
                 double animValue = _bubbleOffsetController.value;
                 return Stack(
                   children: [
@@ -76,7 +89,7 @@ class _GeneralInfoPageState extends State<GeneralInfoPage> with SingleTickerProv
   }
 
   void getAllPathsFromBloc() {
-    RandomPathBloc randomPathBloc = Provider.of<RandomPathBloc>(context, listen: false);
+    randomPathBloc = Provider.of<RandomPathBloc>(context, listen: false);
 
     randomPathBloc.add(RandomPathGeneratorEvent(windowSize: MediaQuery.of(context).size, numberOfPaths: 20));
 
@@ -85,7 +98,7 @@ class _GeneralInfoPageState extends State<GeneralInfoPage> with SingleTickerProv
   Offset _getPathCurveDetails({ @required Path path, @required double animValue}) {
     PathMetrics pathMetrics = path.computeMetrics();
     PathMetric pathMetric = pathMetrics.elementAt(0);
-    double valueToTranslateTo = pathMetric.length * (animValue / 2);
+    double valueToTranslateTo = pathMetric.length * animValue;
     Tangent positionDetailsForBubble = pathMetric.getTangentForOffset(valueToTranslateTo);
     return positionDetailsForBubble.position;
   }
